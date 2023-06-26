48e06cef93485afbc7c206f77426679a6b4c950f
inott-git <arnst.artyom@gmail.com>
2023-06-26 09:46:35 +0300

    singin_test

--- test/service/auth_test.go
+++ test/service/auth_test.go
@@ -74,3 +74,70 @@ func TestSingUp(t *testing.T) {
 		})
 	}
 }
+
+func TestSingIn(t *testing.T) {
+	type mockBehavior func(r *mock_repository.MockRepo, login string)
+	type mockBehaviorHash func(r *mock_hashFunc.MockHashTools, password string, hash string)
+
+	tests := []struct {
+		name             string
+		inputUser        models.UserDB
+		inputUserDB      models.UserDB
+		mockBehavior     mockBehavior
+		mockBehaviorHash mockBehaviorHash
+		expectedError    error
+		expectedAnswer   string
+	}{
+		{
+			name:        "OK",
+			inputUser:   models.UserDB{Login: "test", Password: "testp"},
+			inputUserDB: models.UserDB{Login: "test", Password: "testp"},
+			mockBehavior: func(r *mock_repository.MockRepo, login string) {
+				r.EXPECT().GetUser(login).Return(models.UserDB{Login: "test", Password: "testp"}, nil)
+			},
+			mockBehaviorHash: func(r *mock_hashFunc.MockHashTools, password, hash string) {
+				r.EXPECT().CheckPasswordHash(password, hash).Return(true)
+			},
+			expectedError: nil,
+		},
+		{
+			name:        "Invalid Password",
+			inputUser:   models.UserDB{Login: "test", Password: "test"},
+			inputUserDB: models.UserDB{Login: "test", Password: "testp"},
+			mockBehavior: func(r *mock_repository.MockRepo, login string) {
+				r.EXPECT().GetUser(login).Return(models.UserDB{Login: "test", Password: "testp"}, nil)
+			},
+			mockBehaviorHash: func(r *mock_hashFunc.MockHashTools, password, hash string) {
+				r.EXPECT().CheckPasswordHash(password, hash).Return(false)
+			},
+			expectedError: errors.New("Passwords is different"),
+		},
+		{
+			name:      "DB Error",
+			inputUser: models.UserDB{Login: "test", Password: "test"},
+			mockBehavior: func(r *mock_repository.MockRepo, login string) {
+				r.EXPECT().GetUser(login).Return(models.UserDB{Login: "", Password: ""}, errors.New("test error"))
+			},
+			mockBehaviorHash: func(r *mock_hashFunc.MockHashTools, password, hash string) {},
+			expectedError:    errors.New("test error"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			repo := mock_repository.NewMockRepo(c)
+			test.mockBehavior(repo, test.inputUser.Login)
+
+			hash := mock_hashFunc.NewMockHashTools(c)
+			test.mockBehaviorHash(hash, test.inputUser.Password, test.inputUserDB.Password)
+
+			services := service.NewServices(repo, hash)
+			_, err := services.AuthServices.SingIn(test.inputUser)
+
+			assert.Equal(t, test.expectedError, err)
+		})
+	}
+}
